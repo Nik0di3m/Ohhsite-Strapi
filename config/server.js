730a4fb15396335c929dd36fdaaeb2dad96498d7
Nik0di3m <norbert.waleszczyk@icloud.com>
2021-05-05 16:48:04 +0200

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9aea1a64a014710a068637258f0c5c5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6b8a2ded-27c0-4e18-a116-b9d582cf693b'
+};
